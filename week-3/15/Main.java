5e5e14bad590c1c6ec58913dae2a7d79aeab481f
malipeddisekhar <malipeddisekhar63@gmail.com>
2026-06-05 14:09:57 +0530

    Working on kadanes algorithm for today crt class

---
+++ week-3/15/Main.java
@@ -0,0 +1,21 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int num : arr)
+        {
+            currentSum += num;
+            maxSum = Math.max(maxSum, currentSum);
+            if (currentSum < 0)
+            {
+                currentSum = 0;
+            }
+        }
+
+        System.out.println("Maximum subarray sum is: " + maxSum);
+    }
+}
