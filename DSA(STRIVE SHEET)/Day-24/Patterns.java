d66cef34211a30c0b6e35fa09207e33692b70f70
malipeddisekhar <malipeddisekhar63@gmail.com>
2026-06-25 22:26:58 +0530

    Understand the Depth of the patterns

--- DSA(STRIVE SHEET)/Day-24/Patterns.java
+++ DSA(STRIVE SHEET)/Day-24/Patterns.java
@@ -5,7 +5,7 @@
 //         int n = 5;
 //         for(int i=1; i<=n; i++)
 //         {
-//             for(int j=1; j<=n; j++)
+//             for(int j=0; j<3; j++)
 //             {
 //                 System.out.print("* ");
 //             }
@@ -137,4 +137,112 @@
 // * * * 
 // * * 
 // * 
+//============================================================
+// class Patterns
+// {
+//     public static void main(String[] args)
+//     {
+//         int n=5;
+//         for(int i=0;i<n;i++)
+//         {
+//             //space 
+//             for(int j=0;j<=n-i-1;j++)
+//             {
+//                 System.out.print(" ");
+//             }
+//             //star
+//             for(int j=0;j<2*i+1;j++) 
+//             {
+//                 System.out.print("*");
+//             }
+//             //space
+//             for(int j=0;j<n-i-1;j++)
+//             {
+//                 System.out.print(" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+// //output:
+//      *    
+//     ***   
+//    *****  
+//   ******* 
+//  *********
+//============================================================
+// class 
+// {
+//     public static void main(String[] args)
+//     {
+//      int n=5;
+//              for(int i=0;i<n;i++)
+//         {
+//             //space 
+//             for(int j=0;j<=n-i-1;j++)
+//             {
+//                 System.out.print(" ");
+//             }
+//             //star
+//             for(int j=0;j<2*i+1;j++) 
+//             {
+//                 System.out.print("*");
+//             }
+//             //space
+//             for(int j=0;j<n-i-1;j++)
+//             {
+//                 System.out.print(" ");
+//             }
+//             System.out.println();
+//         }
+//      for(int i=0;i<n;i++)
+//         {
+//             //space 
+//             for(int j=0;j<i;j++)
+//             {
+//                 System.out.print(" ");
+//             }
+//             //star
+//             for(int j=0;j<2*n-2*i-1;j++) 
+//             {
+//                 System.out.print("*");
+//             }
+//             //space
+//             for(int j=0;j<i;j++)
+//             {
+//                 System.out.print(" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+//============================================================
+// class Patterns
+// {
+//     public static void main(String[] args)
+//     {
+//         int n=5;
+//         int star=1;
+//         for(int i=0;i<n;i++)
+//         {
+//           if(i%2==0)
+//           {
+//             for(int j=0;j<star;j++)
+//             {
+//                 System.out.print("*");
+//             }
+//             star++;
+//           }
+//           else
+//           {
+//             for(int j=0;j<star;j++)
+//             {
+//                 System.out.print("#");
+//             }
+//             star++;
+//           }
+//             System.out.println();
+//         }
+//     }
+// }
 //============================================================
