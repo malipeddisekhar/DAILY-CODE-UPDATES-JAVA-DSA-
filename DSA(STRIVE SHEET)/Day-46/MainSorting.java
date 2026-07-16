a066cf29e031c7d9b0b57e6d1a4f2661b9f4337a
Malipeddi Sekhar <malipeddisekhar63@gmail.com>
2026-07-16 09:28:08 +0530

    Some progress

--- DSA(STRIVE SHEET)/Day-46/MainSorting.java
+++ DSA(STRIVE SHEET)/Day-46/MainSorting.java
@@ -1,34 +1,68 @@
-//Bubble sorting
+// //Bubble sorting
+// class MainSorting
+// {
+//     public static void main(String[] args)
+//     {
+//         int n[] = {5, 20, 8, 4, 12, 6};
+//         System.out.println("Original array:");
+//         for(int i=0; i<n.length; i++)
+//         {
+//             System.out.print(n[i]+" ");
+//         }
+//          for(int i=0; i<n.length; i++)
+//          {
+//                 for(int j=0; j<n.length-i-1; j++)
+//                 {
+//                     if(n[j]>n[j+1])
+//                     {
+//                         int temp = n[j];
+//                         n[j] = n[j+1];
+//                         n[j+1] = temp;
+//                     }
+//                 }
+//          }
+        
+//     }
+//     public static void printArray(int arr[])
+//     {
+//         System.out.println("\nSorted array:");
+//         for(int i=0; i<arr.length; i++)
+//         {
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
+
+import java.util.Scanner;
+
 class MainSorting
 {
     public static void main(String[] args)
     {
-        int n[] = {5, 20, 8, 4, 12, 6};
-        System.out.println("Original array:");
-        for(int i=0; i<n.length; i++)
+        Scanner sc=new Scanner(System.in);
         {
-            System.out.print(n[i]+" ");
-        }
-         for(int i=0; i<n.length; i++)
-         {
-                for(int j=0; j<n.length-i-1; j++)
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            for(int i=0;i<n;i++)
+            {
+                arr[i]=sc.nextInt();
+            }
+            int max=arr[0];
+            int second=-1;
+            for(int i=1;i<n;i++)
+            {
+                if(arr[i]>max)
                 {
-                    if(n[j]>n[j+1])
-                    {
-                        int temp = n[j];
-                        n[j] = n[j+1];
-                        n[j+1] = temp;
-                    }
+                    second =max;
+                    max=arr[i];
                 }
-         }
-        
-    }
-    public static void printArray(int arr[])
-    {
-        System.out.println("\nSorted array:");
-        for(int i=0; i<arr.length; i++)
-        {
-            System.out.print(arr[i]+" ");
+             else if(arr[i]>second&&arr[i]<max)
+            {
+                second=arr[i];
+            }
+            }
+            System.out.println(second);
+            
         }
     }
 }
