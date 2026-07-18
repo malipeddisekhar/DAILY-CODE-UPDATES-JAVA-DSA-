0cb498bbe08ba39f8944309d8ebdf31de10deb1f
Malipeddi Sekhar <malipeddisekhar63@gmail.com>
2026-07-18 11:30:05 +0530

    Some leetcode

---
+++ DSA(STRIVE SHEET)/Day-48/Leetcode.java
@@ -0,0 +1,41 @@
+
+// X of a Kind in a Deck of Cards 
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+
+        Arrays.sort(deck);
+
+        int gcd = 0;
+        int count = 1;
+
+        for (int i = 1; i < deck.length; i++) {
+
+            if (deck[i] == deck[i - 1]) {
+                count++;
+            } else {
+                gcd = findGCD(gcd, count);
+                count = 1;
+            }
+        }
+
+        // Last group's frequency
+        gcd = findGCD(gcd, count);
+
+        return gcd >= 2;
+    }
+
+    public int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+} 
+
+
+// using these we can found the optimal solution for these problems.
