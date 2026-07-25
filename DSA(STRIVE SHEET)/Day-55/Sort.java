f284f994537288c2ba4ff33ca5e3a8d9e5edd3af
Malipeddi Sekhar <malipeddisekhar63@gmail.com>
2026-07-25 21:45:03 +0530

    Optimized solution

--- DSA(STRIVE SHEET)/Day-55/Sort.java
+++ DSA(STRIVE SHEET)/Day-55/Sort.java
@@ -1,11 +1,40 @@
-class Sort
+// class Sort
+// {
+//     public static void main(String[] args)
+//     {
+//         int arr[]={5,4,3,2,1};
+//         int n=arr.length;
+//         for(int i=0;i<n-1;i++)
+//         {
+//             for(int j=0;j<n-i-1;j++)
+//             {
+//                 if(arr[j]>arr[j+1])
+//                 {
+//                     int temp=arr[j];
+//                     arr[j]=arr[j+1];
+//                     arr[j+1]=temp;
+//                 }
+//             }
+//         }
+//         for(int i=0;i<n;i++)
+//         {
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
+
+
+
+
+class Main
 {
     public static void main(String[] args)
     {
-        int arr[]={5,4,3,2,1};
+        int arr[]={6,3,7,3,7};
         int n=arr.length;
         for(int i=0;i<n-1;i++)
         {
+            boolean swapped=false;
             for(int j=0;j<n-i-1;j++)
             {
                 if(arr[j]>arr[j+1])
@@ -15,8 +44,12 @@ class Sort
                     arr[j+1]=temp;
                 }
             }
+            if(swapped==false)
+            {
+                break;
+            }
         }
-        for(int i=0;i<n;i++)
+        for(int i=0;i<arr.length;i++)
         {
             System.out.print(arr[i]+" ");
         }
